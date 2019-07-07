7f2912cf92b3154664d9fd92897acadd24008a1a
Alexander Nikolskiy <alexxxnikolskiy@gmail.com>
2019-07-07 16:31:41 +0300

    Fix 'Primitive Calculator' problem

--- algorithmic-toolbox/dynamic-programming/primitive-calculator/index.js
+++ algorithmic-toolbox/dynamic-programming/primitive-calculator/index.js
@@ -8,16 +8,15 @@ process.stdin.setEncoding('utf8');
 rl.on('line', readLine);
 
 function readLine(line) {
-    const [minOper, path] = min(parseInt(line, 10));
+    const [minOper, res] = min(parseInt(line, 10));
 
     console.log(minOper);
-    console.log(path.join(' '));
+    console.log(res.join(' '));
     process.exit();
 }
 
 function min(number) {
     const minOper = [];
-    const path = [];
 
     minOper[0] = minOper[1] = 0;
 
@@ -37,32 +36,37 @@ function min(number) {
         }
     }
 
-    let n = number;
-    path.push(number);
+    return [minOper[number], sequence(number, minOper)];
+}
+
+function sequence(n, minOper = []) {
+    const res = [];
+
+    res.push(n);
 
     while (n > 1) {
         const val1 = minOper[n - 1];
-        const val2 = n % 2 === 0 ? minOper[n / 2] : Infinity;
-        const val3 = n % 3 === 0 ? minOper[n / 3] : Infinity;
+        const val2 = n % 2 === 0 ? minOper[n / 2] : Number.POSITIVE_INFINITY;
+        const val3 = n % 3 === 0 ? minOper[n / 3] : Number.POSITIVE_INFINITY;
         const val = Math.min(val1, val2, val3);
 
         switch (val) {
             case val1:
-                path.push(n - 1);
+                res.push(n - 1);
                 n--;
                 break;
             case val2:
-                path.push(n / 2);
+                res.push(n / 2);
                 n /= 2;
                 break;
             case val3:
-                path.push(n / 3);
+                res.push(n / 3);
                 n /= 3;
                 break;
         }
     }
 
-    return [minOper[number], path.reverse()];
+    return res.reverse();
 }
 
 module.exports = min;
